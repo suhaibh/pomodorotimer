f954691b2785edd4cd668a3313a280ba8140c4ab
suhaibh <suhaib.habibullah@gmail.com>
2016-01-03 18:36:01 -0500

    Create javascript file for Pomodoro timer project

---
+++ pomodorotimer.js
@@ -0,0 +1,14 @@
+var startTime = 25
+window.onLoad(setTimer())
+function setTimer(){
+	timer = document.getElementById("timer");
+	timer.innerHTML = startTime;
+}
+
+function startTimer(){
+
+}
+
+function stopTimer(){
+
+}
