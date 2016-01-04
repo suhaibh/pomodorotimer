fcc235e924d0ad265bc7d15f89509a58a51107af
suhaibh <suhaib.habibullah@gmail.com>
2016-01-04 00:18:25 -0500

    Add increase and decrease timer js functions

---
+++ pomodoroctimer.js
@@ -0,0 +1,13 @@
+var startTime = 25
+function setTimer(){
+	timer = document.getElementById("timer");
+	timer.innerHTML = startTime;
+}
+
+function startTimer(){
+
+}
+
+function stopTimer(){
+
+}
--- pomodorotimer.js
+++ pomodorotimer.js
@@ -1,9 +1,25 @@
 var startTime = 25
-window.onLoad(setTimer())
+window.onload = setTimer;
 function setTimer(){
 	timer = document.getElementById("timer");
 	timer.innerHTML = startTime;
 }
+
+function increaseTimer(){
+	startTime++;
+	timer.innerHTML = startTime;
+}
+
+function decreaseTimer(){
+	startTime--;
+	timer.innerHTML = startTime;
+}
+
+var increaseTimerButton = document.getElementById("increaseTimerButton");
+increaseTimerButton.onclick = increaseTimer;
+
+var decreaseTimerButton = document.getElementById("decreaseTimerButton");
+decreaseTimerButton.onclick = decreaseTimer;
 
 function startTimer(){
 
